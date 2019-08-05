e8a3cb7870acb8f3df6e28a7ac7044782141dcfa
Flora <caponi.flora@gmail.com>
2019-08-05 17:57:45 +0200

    first_commit

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,24 @@
+class StaticController < ApplicationController
+	def team
+	end 
+
+	def contact
+	end 
+
+	def login
+		@user = User.new
+		@user.first_name = params[:first_name]
+
+		@gossips = GossipItem.all
+	end 
+
+	def user
+		@user = User.new
+		@user.first_name = params[:first_name]
+	end 
+
+	def show
+    @gossip = GossipItem.find(params['id'])
+	end 
+
+end 
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get '/team', to: 'static#team'
+  get '/contact', to: 'static#contact'
+
+  get '/', to: 'static#login'
+
+  get '/welcome/:id', to: 'static#user' do 
+  	erb :welcome_user, locals: {id: params['first_name']}
+  end 
+
+  get 'gossips/:id', to: 'static#show' do 
+  	erb :show, locals: {id: params['id']}
+  end 
+
+end
